0f518c51995c11ba0b84b535bbd4869047c31637
Aline Nishida <keminishida@gmail.com>
2025-09-21 12:42:44 -0300

    Commit inicial

---
+++ ClienteEnvia/ClienteEnvia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ClienteEnvia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketenviar = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            IPEndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+
+            Console.ReadKey();
+            socketenviar.SendTo(Encoding.ASCII.GetBytes("Mensagem de Teste: 123 testando!"), endereco);
+            Console.ReadKey();
+            socketenviar.SendTo(Encoding.ASCII.GetBytes("...outra mensagem de teste!"), endereco);
+            socketenviar.Close();
+         }
+    }
+}
---
+++ ServidorRecebe/ServidorRecebe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServidorRecebe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketreceber = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.IP);
+            EndPoint endereco = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9060);
+            byte[] data = new byte[1024];
+            int  qtdbytes;
+
+            socketreceber.Bind(endereco);
+            while (true)
+            {
+                 qtdbytes = socketreceber.ReceiveFrom(data, ref endereco);
+                 Console.WriteLine(Encoding.ASCII.GetString(data, 0, qtdbytes));
+            }
+        }
+    }
+}
